b04f8bff9bd49899c7a40fbed576935a7bc6466a
Mark Tomicki <marktomicki@gmail.com>
2014-04-17 08:43:00 +0200

    ignore

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'lolquest',
+			'username'  => 'lolquest',
+			'password'  => 'yhV4wBu77GKTK9UN',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
