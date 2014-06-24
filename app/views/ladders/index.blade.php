45e0be17930665b2df94cf9ba1dcc66a4f207ba1
Mark Tomicki <marktomicki@gmail.com>
2014-06-24 10:01:29 +0200

    ladder icon

--- app/views/ladders/index.blade.php
+++ app/views/ladders/index.blade.php
@@ -17,13 +17,28 @@
 	<table class="table table-striped">
 		<tr>
 			<th>{{ trans("ladders.rang") }}</th>
-			<th colspan="2">{{ trans("ladders.summoner") }}</th>
+			<th colspan="3">{{ trans("ladders.summoner") }}</th>
 			<th>{{ trans("ladders.quests") }}</th>
 			<th>{{ trans("ladders.total_exp") }}</th>
 		</tr>
 	@foreach($ladder as $row)
 		<tr>
 			<td width="60">{{ $row->rang }}.</td>
+			<td width="20">
+				@if($row->rang <= 3)
+					<img src="/img/leagues/challenger_5.png" height="20" />
+				@elseif($row->rang <= 10)
+					<img src="/img/leagues/diamond_5.png" height="20" />
+				@elseif($row->rang <= 25)
+					<img src="/img/leagues/platinum_5.png" height="20" />
+				@elseif($row->rang <= 50)
+					<img src="/img/leagues/gold_5.png" height="20" />	
+				@elseif($row->rang <= 100)
+					<img src="/img/leagues/silver_5.png" height="20" />
+				@elseif($row->rang >= 101)
+					<img src="/img/leagues/bronze_5.png" height="20" />
+				@endif
+			</td>
 			<td width="20"><a href="/summoner/{{ $row->user->region }}/{{ $row->user->summoner_name }}"><img src="/img/profileicons/profileIcon{{ $row->user->summoner->profileIconId }}.jpg" class="img-circle" width="20" /></a></td>
 			<td><a href="/summoner/{{ $row->user->region }}/{{ $row->user->summoner_name }}">{{ $row->user->summoner_name }}</a></td>
 			<td>{{ $row->total_quests }}</td>
