627cb53d0191af10d36bf617223f0fe7f55a4739
Mark Tomicki <marktomicki@gmail.com>
2014-06-26 15:25:01 +0200

    Team page

--- app/views/pages/team.blade.php
+++ app/views/pages/team.blade.php
@@ -3,12 +3,31 @@
 @section('content')
 	<br/>
 	<div class="faq">
-	
-		<h3>Mark Tomicki</h3>
-		Concept, Design, Coding
-		<br/><br/><br/>
-		<h3>Klaus Brandt</h3>
-		Concept, Coding
 		
+		<table>
+			<tr>
+				<td valign="top" width="120">
+					<img src="/img/team/mark.jpg" class="img-circle" alt="Mark Tomicki" />
+				</td>
+				<td>
+					<h3>Mark Tomicki</h3>
+					<a href="mailto: marktomicki@gmail.com">marktomicki@gmail.com</a><br/>
+					Concept, Design, Coding<br/>
+					<br/>
+				</td>
+			</tr>
+			<tr>
+				<td valign="top" width="120">
+					<br/>
+					<img src="/img/team/klaus.jpg" class="img-circle" alt="Klaus Brandt" />
+				</td>
+				<td>
+					<br/>
+					<h3>Klaus Brandt</h3>
+					<a href="mailto: dualdeath@gmail.com">dualdeath@gmail.com</a><br/>
+					Concept, Coding
+				</td>
+			</tr>
+		</table>		
 	</div>
 @stop
