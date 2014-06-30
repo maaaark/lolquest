1dc489d4a75da13f2257743cafef0d7a123fdfd8
Mark Tomicki <marktomicki@gmail.com>
2014-06-30 16:08:55 +0200

    shop

--- app/views/shop/index.blade.php
+++ app/views/shop/index.blade.php
@@ -2,38 +2,13 @@
 @section('title', trans("shop.shop"))
 @section('content')
 	<br/>
-	<table>
+	<table width="100%">
 		<tr>
 			<td valign="top" width="20%">
 				@include('shop.sidebar')
 			</td>
 			<td valign="top" width="80%">
-				<div class="col-lg-4 col-md-4 col-sm-6 col-sm-6 col-xs-6">
-						<div class="product_skin" style="background: url('/img/skins/pax_jax.jpg')">
-							<div class="skin_description">
-								<h3>Pax Jax</h3>
-								<a href="#" class="btn btn-primary" data-toggle="modal" data-target="#myModal-6">Buy for 25000 QP</a>
-							</div>
-						</div>
-					</div>
-					
-					<div class="col-lg-4 col-md-4 col-sm-6 col-sm-6 col-xs-6">
-						<div class="product_skin" style="background: url('/img/skins/championship_thresh.jpg')">
-							<div class="skin_description">
-								<h3>Championship Thresh</h3>
-								<a href="#" class="btn btn-primary" data-toggle="modal" data-target="#myModal-6">Buy for 25000 QP</a>
-							</div>
-						</div>
-					</div>
-					
-					<div class="col-lg-4 col-md-4 col-sm-6 col-sm-6 col-xs-6">
-						<div class="product_skin" style="background: url('/img/skins/pax_sivir.jpg')">
-							<div class="skin_description">
-								<h3>Pax Sivir</h3>
-								<a href="#" class="btn btn-primary" data-toggle="modal" data-target="#myModal-6">Buy for 25000 QP</a>
-							</div>
-						</div>
-					</div>
+
 				@foreach($products as $product)
 					
 				
--- app/views/shop/sidebar.blade.php
+++ app/views/shop/sidebar.blade.php
@@ -1,9 +1,9 @@
 <ul class="nav nav-pills nav-stacked">
   <li><a href="/shop/">{{ trans("shop.offers") }}</a></li>
-  <li class="buy_qp"><a href="/shop/buy_qp">{{ trans("shop.buy_qp") }}</a></li>
+  <!--<li class="buy_qp"><a href="/shop/buy_qp">{{ trans("shop.buy_qp") }}</a></li>
   <li><a href="/shop/backgrounds">{{ trans("shop.backgrounds") }}</a></li>
   <li><a href="/shop/riot_points">{{ trans("shop.rp") }}</a></li>
   <li><a href="/shop/ep_boosts">{{ trans("shop.ep_boost") }}</a></li>
-  <li><a href="/shop/skins">{{ trans("shop.skins") }}</a></li>
+  <li><a href="/shop/skins">{{ trans("shop.skins") }}</a></li>-->
   <li><a href="/shop/history">{{ trans("shop.history") }}</a></li>
 </ul>
