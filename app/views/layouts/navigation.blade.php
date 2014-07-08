d5147992248b6d118b721c6d1f8ca0ed72c2eb4d
Mark Tomicki <marktomicki@gmail.com>
2014-07-08 11:13:01 +0200

    rules and ladder icon

--- app/views/layouts/navigation.blade.php
+++ app/views/layouts/navigation.blade.php
@@ -3,7 +3,7 @@
 	<li><a href="/challenges"><span class="glyphicon glyphicon-certificate"></span> <span class="hidden-xs hidden-sm">Challenges</span></a></li>
 	<li><a href="/timeline"><span class="glyphicon glyphicon-dashboard"></span> <span class="hidden-xs hidden-sm">Timeline</span></a></li>
 	<li class=""><a href="/shop"><span class="glyphicon glyphicon-shopping-cart"></span> <span class="hidden-xs hidden-sm">Shop</span></a></li>			
-	<li><a href="/ladders"><span class="glyphicon glyphicon-list"> </span> <span class="hidden-xs hidden-sm">Ladders</span></a></li>
+	<li><a href="/ladders"><i class="fa fa-trophy"></i> <span class="hidden-xs hidden-sm">Ladders</span></a></li>
 	<li><a href="/champions"><span class="glyphicon glyphicon-list"> </span> <span class="hidden-xs hidden-sm">Champions</span></a></li>
 	<li><a href="/blogs"><i class="fa fa-file"></i> <span class="hidden-xs hidden-sm">Blog</span></a></li>
 	<li><a href="/forum"><i class="fa fa-folder-open"></i> <span class="hidden-xs hidden-sm">Forum</span></a></li>
--- app/views/pages/rules.blade.php
+++ app/views/pages/rules.blade.php
@@ -2,6 +2,13 @@
 @section('title', 'Rules')
 @section('content')
 	<br/>
+	<h3>Register</h3>
+	<p>
+		To be part of the ladder, you can <a href="/register"><strong>register your own account here.</strong></a><br/>
+		It is free and everyone can participate. Your summoner must be verified in order to be part of the ladder.<br/>
+		<h4><a href="/register">Register now</a></h4>
+	</p>
+	<br/>
 	<h3>Map and Games</h3>
 	<p>
 		All Games have to be on <strong>Summoners Rift!</strong> All other games won't count.<br/>
@@ -17,13 +24,9 @@
 			<li>Rang 11-15: 1600 RP</li>
 			<li>Rang 16-20: 800 RP</li>
 		</ul>
-		The winner of the monthly ladder will be selected at the beginning of the next month. Example: Winner of January ladder will be announced in February. Also the prices will be sent out in February then.
+		The winner of the monthly ladder will be selected at the beginning of the next month. Example: Winner of January ladder will be announced in February. Also the prices will be sent out in February then.<br/>
+		<br/>
+		A normal ladder will start at the 1st of a month and hold on until the last day of the month.
 	</p>
-	<br/>
-	<h3>Register</h3>
-	<p>
-		To be part of the ladder, you can <a href="/register">register your own account here.</a><br/>
-		It is free and everyone can participate.<br/>
-		Your summoner must be verified in order to be part of the ladder.
-	</p>
+	
 @stop
