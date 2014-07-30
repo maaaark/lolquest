5f64c88e79487317562231c011d7d22b7e64ef55
klausbrandt <dualdeath@gmail.com>
2014-07-30 21:05:59 +0200

    fix achievements + fix notifi

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -739,7 +739,7 @@ class UsersController extends \BaseController {
 				$user_friend = Auth::user();
 				$user_friend->friends()->attach($id);
 				$user = User::findOrFail($id);
-				$user->notify(3, '<a href="/summoner/'.$user_friend->region.'/'.$user_friend->summoner_name.'">'.$user_friend->summoner_name.'</a> '.trans("friends.add").' <a href="/accept_friend/'.$user_friend->id.'">'.trans("friends.accept_noti").'</a> <a href="/remove_friend/'.$user_friend->id.'">'.trans("friends.reject").'</a>');
+				$user->notify(3, '<a href="/summoner/'.$user_friend->region.'/'.$user_friend->summoner_name.'">'.$user_friend->summoner_name.'</a> '.trans("friends.add").'</br> <a href="/accept_friend/'.$user_friend->id.'">'.trans("friends.accept_noti").'</a>   <a href="/remove_friend/'.$user_friend->id.'">'.trans("friends.reject").'</a>');
 				$model = new FriendUser;
 				$model->setTable("friend_users");
 				$notify = $model->where("friend_id","=", $id)->where('user_id','=', $user_friend->id)->first();;
