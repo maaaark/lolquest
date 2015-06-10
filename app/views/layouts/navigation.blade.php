24c3f383be2648311187745bc9b22e281cbe7a2b
Mark Tomicki <marktomicki@gmail.com>
2015-06-10 09:34:47 +0200

    fix

--- app/views/layouts/navigation.blade.php
+++ app/views/layouts/navigation.blade.php
@@ -1,11 +1,11 @@
 <ul>
 	<li><a href="/dashboard"><span class="glyphicon glyphicon-dashboard"></span> <span class="hidden-xs hidden-sm">Quests</span></a></li>
 	<li><a href="/challenges"><span class="glyphicon glyphicon-certificate"></span> <span class="hidden-xs hidden-sm">Challenges</span></a></li>
-    <li><a href="/lottery"><i class="fa fa-group"></i> <span class="hidden-xs hidden-sm">Lottery</span></a></li>
     <li><a href="/timeline"><span class="glyphicon glyphicon-dashboard"></span> <span class="hidden-xs hidden-sm">Timeline</span></a></li>
 	<li><a href="/shop"><span class="glyphicon glyphicon-shopping-cart"></span> <span class="hidden-xs hidden-sm">Shop</span></a></li>
 	<li><a href="/ladders"><i class="fa fa-trophy"></i> <span class="hidden-xs hidden-sm">Ladders</span></a></li>
-    <!--<li><a href="/ladders/top100"><i class="fa fa-trophy"></i> <span class="hidden-xs hidden-sm">Top 100</span></a></li>
+    <li><a href="/ladders/top100"><i class="fa fa-trophy"></i> <span class="hidden-xs hidden-sm">Top 100</span></a></li>
+    <!--<li><a href="/lottery"><i class="fa fa-group"></i> <span class="hidden-xs hidden-sm">Lottery</span></a></li>
     <li><a href="/champions"><span class="glyphicon glyphicon-list"> </span> <span class="hidden-xs hidden-sm">Champions</span></a></li>-->
 	<li><a href="/forum"><i class="fa fa-folder-open"></i> <span class="hidden-xs hidden-sm">Forum</span></a></li>
 </ul>
